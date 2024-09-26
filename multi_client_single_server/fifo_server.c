8e725a91cd0af8ae5f125c657f484461b93c3e30
Robert Smith <robertjamessmith61@gmail.com>
2024-09-26 15:43:36 +1000

    started creating server.

--- multi_client_single_server/fifo_server.c
+++ multi_client_single_server/fifo_server.c
@@ -11,20 +11,33 @@
 
 // https://www.cs.cmu.edu/afs/cs/academic/class/15492-f07/www/pthreads.html
 
-void pipe_listener(void *arg);
+// -- Constants
+const long bufLen = ChunkSize * sizeof(char);
+// --
+
+// -- Global variables
+// ---- Listener variables
+int listenerFd;
+char *listenerData;
+const char *listenerName;
+// ----
+// --
+
+// -- Function Declarations
+// ---- Listener pipe function declarations
+static void *pipe_listener(void *arg);
+static void *pipe_listener_cleanup(void *arg);
+// ----
+// --
 
 int main(int argc, char *argv[])
 {
-    const char *rxName = "./fifo_multi_Rx";
+    const char *rxName = "./fifo_listener";
 
-    const long bufLen = ChunkSize * sizeof(char);
+    pthread_t listenerThread;
+    int listenerReturn;
 
-    char *rxData = (char *)calloc(ChunkSize, sizeof(char));
-
-    mkfifo(rxName, 0666);                        /* read/write for user/group/others */
-    int rxFd = open(rxName, O_CREAT | O_RDONLY); /* open as read-only */
-    if (rxFd < 0)
-        return -1; /** error **/
+    listenerReturn = pthread_create(&listenerThread, NULL, pipe_listener, (void *)rxName);
 
     while (1)
     {
@@ -41,10 +54,46 @@ int main(int argc, char *argv[])
         ssize_t txCount = write(txFd, txData, strcspn(txData, "\0"));
     }
 
-    close(rxFd);
-    unlink(rxName);
-
-    free(rxData);
+    pthread_join(listenerThread, NULL);
 
     return 0;
+}
+
+static void *pipe_listener(void *arg)
+{
+    // Add our cleanup routine so everything is taken care of if we get cancelled or end gracefully
+    pthread_cleanup_push(pipe_listener_cleanup, NULL);
+
+    // Grab listener pipe name from arg
+    listenerName = (char *)arg;
+
+    // Allocate rx buffer
+    listenerData = (char *)calloc(ChunkSize, sizeof(char));
+
+    // Create named pipe
+    mkfifo(listenerName, 0666);                          /* read/write for user/group/others */
+    listenerFd = open(listenerName, O_CREAT | O_RDONLY); /* open as read-only */
+    if (listenerFd < 0)
+        return -1; /** error **/
+
+    while (1)
+    {
+        ssize_t count = read(listenerFd, listenerData, bufLen);
+
+        if (count <= 0)
+            break; /* end of stream */
+        else
+            printf("recieved:\n%s", listenerData);
+    }
+
+    return 0;
+}
+
+static void *pipe_listener_cleanup(void *arg)
+{
+    // Close pipe when we're done
+    close(listenerFd);
+    unlink(listenerName);
+
+    free(listenerData);
 }
