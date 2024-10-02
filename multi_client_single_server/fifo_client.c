fe922fd7eeb5c75cf5e7ca3909c1b6728b8378d4
Robert Smith <robertjamessmith61@gmail.com>
2024-10-02 14:53:29 +1000

    Added cleanup code to fifo_server Started copying code across to fifo_client

--- multi_client_single_server/fifo_client.c
+++ multi_client_single_server/fifo_client.c
@@ -6,27 +6,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+#include <errno.h>
 #include "fifo_common.h"
+
+// >> Constants
+const long bufLen = CHUNKSIZE * sizeof(char);
+const char *txName = "./fifo_listener";
+const char *cmdSubscribe = "subscribe";
+// <<
+
+// >> Global variables
+// >>>> Listener variables
+int listenerFd;
+char *listenerData;
+char *listenerName;
+// <<<<
+// <<
+
+// >> Function Declarations
+// >>>> Listener pipe function declarations
+static int *pipe_listener(void *arg);
+static int *pipe_listener_cleanup(void *arg);
+// <<<<
+// <<
 
 int main(int argc, char *argv[])
 {
-    const char *txName = "./fifo_multi_Tx";
-    const char *rxName = "./fifo_multi_Rx";
+    char rxName[MAX_PIPE_NAME_LEN];
 
-    const long bufLen = CHUNKSIZE * sizeof(char);
+    int subNameLen = snprintf(rxName, MAX_PIPE_NAME_LEN, "./fifo_sub_%i", getpid());
 
-    char *txData = (char *)calloc(CHUNKSIZE, sizeof(char));
-    char *rxData = (char *)calloc(CHUNKSIZE, sizeof(char));
+    if (subNameLen < 0 || subNameLen > MAX_PIPE_NAME_LEN)
+    {
+        fprintf(stderr, "error: subscriber pipe name length is greater than %i: %s\n",
+                MAX_PIPE_NAME_LEN - 1, rxName);
+    }
+
+    pthread_t listenerThread;
+    int listenerReturn;
+
+    listenerReturn = pthread_create(&listenerThread, NULL, pipe_listener, (void *)rxName);
+
+    char *senderData;
+    senderData = (char *)calloc(CHUNKSIZE, sizeof(char));
 
     mkfifo(txName, 0666);                        /* read/write for user/group/others */
-    int txFd = open(txName, O_CREAT | O_RDONLY); /* open as write-only */
+    int txFd = open(txName, O_CREAT | O_WRONLY); /* open as write-only */
     if (txFd < 0)
-        return -1; /** error **/
-
-    mkfifo(rxName, 0666);                        /* read/write for user/group/others */
-    int rxFd = open(rxName, O_CREAT | O_WRONLY); /* open as read-only */
-    if (rxFd < 0)
-        return -1; /** error **/
+    {
+        int error = errno;
+        fprintf(stderr, "Failed to create named pipe: %s\n", txName);
+        return error;
+    }
 
     while (1)
     {
@@ -44,7 +76,7 @@ int main(int argc, char *argv[])
     unlink(txName); /* unlink from the implementing file */
 
     close(rxFd);
-    unlink(rxName);
+    unlink(listenerName);
 
     free(txData);
     free(rxData);
--- multi_client_single_server/fifo_server.c
+++ multi_client_single_server/fifo_server.c
@@ -13,44 +13,48 @@
 
 // https://www.cs.cmu.edu/afs/cs/academic/class/15492-f07/www/pthreads.html
 
-// -- Defines
+// >> Defines
 #define MAX_CONNECTIONS 32
-// --
+// <<
 
-// -- Constants
+// >> Constants
 const long bufLen = CHUNKSIZE * sizeof(char);
+const char *rxName = "./fifo_listener";
 const char *cmdSubscribe = "subscribe";
-// --
+// <<
 
-// -- Global variables
-// ---- Listener variables
+// >> Global variables
+// >>>> Listener variables
 int listenerFd;
 char *listenerData;
-const char *listenerName;
-// ----
-// ---- Sender variables
-LinkedList *subscibers;
-// --
+char *listenerName;
+// <<<<
 
-// -- Function Declarations
-// ---- Listener pipe function declarations
-static int *pipe_listener(void *arg);
-static int *pipe_listener_cleanup(void *arg);
-// ----
-// --
+// >>>> Sender variables
+LinkedList *subscibers;
+// <<<<
+// <<
+
+// >> Function Declarations
+// >>>> Listener pipe function declarations
+static int *PipeListener(void *arg);
+static int *PipeListenerCleanup(void *arg);
+static void CleanupSubsciberNode(Node *subscriberNode);
+// <<<<
+// <<
 
 int main(int argc, char *argv[])
 {
-    const char *rxName = "./fifo_listener";
-
     // Assign some memory to hold our list of subscribers
     subscibers = (LinkedList *)calloc(1, sizeof(LinkedList));
 
+    // Create our separate thread to handle our listener pipe
     pthread_t listenerThread;
     int listenerReturn;
 
-    listenerReturn = pthread_create(&listenerThread, NULL, pipe_listener, (void *)rxName);
+    listenerReturn = pthread_create(&listenerThread, NULL, PipeListener, (void *)rxName);
 
+    // allocate a buffer for our data we are going to push to subscribers
     char *senderData;
     senderData = (char *)calloc(CHUNKSIZE, sizeof(char));
 
@@ -66,13 +70,11 @@ int main(int argc, char *argv[])
         printf("Message:\n");
         printf("%s", senderData);
 
-        // ssize_t txCount = write(txFd, senderData, strcspn(senderData, "\0"));
-
         Node *subscriberNode = subscibers->first;
 
         while (subscriberNode != NULL)
         {
-            ssize_t txCount = write((*(int *)subscriberNode->data), senderData, strcspn(senderData, "\0"));
+            ssize_t txCount = write(*((int *)subscriberNode->data), senderData, strcspn(senderData, "\0"));
 
             if (txCount > 0)
             {
@@ -81,15 +83,40 @@ int main(int argc, char *argv[])
         }
     }
 
+    int retVal;
+
+    // Tell listenerThread to finish
+    retVal = pthread_cancel(listenerThread);
+    if (retVal < 0)
+    {
+        int error = errno;
+        fprintf(stderr, "Failed to send cancelation request to listener thread\n");
+    }
+
+    // Close all of our connections
+    Node *subscriberNode = subscibers->first;
+
+    while (subscriberNode != NULL)
+    {
+        CleanupSubsciberNode(subscriberNode);
+        DelNodeByAddress(subscibers, subscriberNode);
+        subscriberNode = subscibers->first;
+    }
+
+    // Finally free our memory
+    free(subscibers);
+    free(senderData);
+
+    // Wait for listenerThread to finish cleaning up
     pthread_join(listenerThread, NULL);
 
     return 0;
 }
 
-static int *pipe_listener(void *arg)
+static int *PipeListener(void *arg)
 {
     // Add our cleanup routine so everything is taken care of if we get cancelled or end gracefully
-    pthread_cleanup_push(pipe_listener_cleanup, NULL);
+    pthread_cleanup_push(PipeListenerCleanup, NULL);
 
     // Grab listener pipe name from arg
     listenerName = (char *)arg;
@@ -98,7 +125,7 @@ static int *pipe_listener(void *arg)
     listenerData = (char *)calloc(CHUNKSIZE, sizeof(char));
 
     // Create named pipe
-    mkfifo(listenerName, 0666);                          // read/write permissions for user/group/everyone
+    mkfifo(listenerName, 0666);                        // read/write permissions for user/group/everyone
     listenerFd = open(listenerName, O_CREAT | O_RDWR); // Create if doesn't exist, read-only
     if (listenerFd < 0)
     {
@@ -143,10 +170,10 @@ static int *pipe_listener(void *arg)
                 size_t pipeNameLen = strlen(token);
 
                 // check pipe name isn't too long
-                if (pipeNameLen > MAX_PIPE_NAME_LEN)
+                if (pipeNameLen + 1 > MAX_PIPE_NAME_LEN)
                 {
-                    fprintf(stderr, "error: commmand %s pipe name length is greater than %i: %s\n",
-                            cmdSubscribe, MAX_PIPE_NAME_LEN, token);
+                    fprintf(stderr, "error: commmand %s - pipe name length is greater than %i: %s\n",
+                            cmdSubscribe, MAX_PIPE_NAME_LEN - 1, token);
 
                     continue; // start loop again
                 }
@@ -177,7 +204,7 @@ static int *pipe_listener(void *arg)
     return 0;
 }
 
-static int *pipe_listener_cleanup(void *arg)
+static int *PipeListenerCleanup(void *arg)
 {
     int retVal = 0;
     // Close pipe when we're done
@@ -193,4 +220,19 @@ static int *pipe_listener_cleanup(void *arg)
     free(listenerData);
 
     return 0;
+}
+
+static void CleanupSubsciberNode(Node *subscriberNode)
+{
+    int retVal;
+
+    retVal = close(*((int *)subscriberNode->data));
+    if (retVal < 0)
+    {
+        int error = errno;
+        fprintf(stderr, "Failed to close subscriber file handle: %i\n", *((int *)subscriberNode->data));
+    }
+
+    free(subscriberNode->data);
+    free(subscriberNode->key);
 }
